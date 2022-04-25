b2d64da2115e8d926f1b0fa68c5256c7a6971b22
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-04-25 08:37:37 +0200

    Add project files.

---
+++ 19-04-22 OOP ex 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _19_04_22_OOP_ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welk jaar is het");
+            int year = Convert.ToInt32( Console.ReadLine());
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine("Het is een schrikkeljaar");
+            }
+            else
+            {
+                Console.WriteLine("Geen schrikkeljaar!");
+            }
+        }
+    }
+}
